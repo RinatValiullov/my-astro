ae474c4b5e733aa249c5dc5bd4b8101c48810caf
Rinat Valiullov <4rontender@gmail.com>
2021-11-30 14:58:45 +0500

    add "MyCV" component

---
+++ src/components/MyCV/MyCV.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './MyCV.module.css';
+
+const MyCV = () => {
+  return (
+    <header className={styles.content}>
+      <h1>Ринат Валиуллов</h1>
+      <h2>HTML-верстальщик, Фронтенд разработчик</h2>
+      <a href="/assets/cv.pdf" download="cv.pdf" className={styles.cv}>Скачать резюме</a>
+    </header>
+  );
+};
+
+export { MyCV };
